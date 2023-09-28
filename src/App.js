8eb142ad053768e0a4902b8fa7643211eb95f7fd
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-09-28 10:11:11 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Router, Routes } from "react-router-dom";
+
+import "./reset.css"
+
+function App() {
+  return (
+    <h1>
+      aaaa
+    </h1>
+  )
+}
+
+export default App;
