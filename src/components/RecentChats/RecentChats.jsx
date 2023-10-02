f1ea7d2cc5f3cd9a96c53984dac15cb17f9a91a8
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-10-02 08:37:45 -0600

    structure (recent chats)

--- src/components/RecentChats/RecentChats.jsx
+++ src/components/RecentChats/RecentChats.jsx
@@ -1,9 +1,65 @@
 import React from "react";
 
 const RecentChat = () => {
-    return(<>
-    </>    
-    )
-}
+  return (
+    <>
+      <div className="chats">
+        <h1 className="title__dashboard">Recent chats</h1>
+        <div className="search__container">
+          <input
+            type="search"
+            placeholder="Look a chat..."
+            className="input__search"
+          />
+          <img src="./img/search.png" alt="Search" className="search" />
+        </div>
+        <div className="conversation__container">
+          <div className="chat__container">
+            <img src="./img/user.png" alt="User" className="user__image" />
+            <div className="chat__info">
+              <h1 className="chat__name">CR7</h1>
+              <span className="last__message">*ejemplo de mensaje*</span>
+            </div>
+          </div>
+          <div className="chat__container">
+            <img src="./img/user.png" alt="User" className="user__image" />
+            <div className="chat__info">
+              <h1 className="chat__name">CR7</h1>
+              <span className="last__message">*ejemplo de mensaje*</span>
+            </div>
+          </div>
+          <div className="chat__container">
+            <img src="./img/user.png" alt="User" className="user__image" />
+            <div className="chat__info">
+              <h1 className="chat__name">CR7</h1>
+              <span className="last__message">*ejemplo de mensaje*</span>
+            </div>
+          </div>
+          <div className="chat__container">
+            <img src="./img/user.png" alt="User" className="user__image" />
+            <div className="chat__info">
+              <h1 className="chat__name">CR7</h1>
+              <span className="last__message">*ejemplo de mensaje*</span>
+            </div>
+          </div>
+          <div className="chat__container">
+            <img src="./img/user.png" alt="User" className="user__image" />
+            <div className="chat__info">
+              <h1 className="chat__name">CR7</h1>
+              <span className="last__message">*ejemplo de mensaje*</span>
+            </div>
+          </div>
+          <div className="chat__container">
+            <img src="./img/user.png" alt="User" className="user__image" />
+            <div className="chat__info">
+              <h1 className="chat__name">CR7</h1>
+              <span className="last__message">*ejemplo de mensaje*</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default RecentChat
+export default RecentChat;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,64 +1,14 @@
 import React from "react";
+import RecentChat from "../components/RecentChats/RecentChats";
 
 import "./Dashboard.css"
+
 
 const Dashboard = () => {
     return(
     <>
         <section className="section">
-            <div className="chats">
-                <h1 className="title__dashboard">Recent chats</h1>
-                <div className="search__container">
-                    <input type="search" placeholder="Look a chat..." className="input__search"/>
-                    <img src="./img/search.png" alt="Search" className="search" />
-                </div>
-                <div className="conversation__container">
-                    <div className="chat__container">
-                        <img src="./img/user.png" alt="User" className="user__image" />
-                        <div className="chat__info">
-                            <h1 className="chat__name">CR7</h1>
-                            <span className="last__message">*ejemplo de mensaje*</span>
-                        </div>
-                    </div>
-                    <div className="chat__container">
-                        <img src="./img/user.png" alt="User" className="user__image" />
-                        <div className="chat__info">
-                            <h1 className="chat__name">CR7</h1>
-                            <span className="last__message">*ejemplo de mensaje*</span>
-                        </div>
-                    </div>
-                    <div className="chat__container">
-                        <img src="./img/user.png" alt="User" className="user__image" />
-                        <div className="chat__info">
-                            <h1 className="chat__name">CR7</h1>
-                            <span className="last__message">*ejemplo de mensaje*</span>
-                        </div>
-                    </div>
-                    <div className="chat__container">
-                        <img src="./img/user.png" alt="User" className="user__image" />
-                        <div className="chat__info">
-                            <h1 className="chat__name">CR7</h1>
-                            <span className="last__message">*ejemplo de mensaje*</span>
-                        </div>
-                    </div>
-                    <div className="chat__container">
-                        <img src="./img/user.png" alt="User" className="user__image" />
-                        <div className="chat__info">
-                            <h1 className="chat__name">CR7</h1>
-                            <span className="last__message">*ejemplo de mensaje*</span>
-                        </div>
-                    </div>
-                    <div className="chat__container">
-                        <img src="./img/user.png" alt="User" className="user__image" />
-                        <div className="chat__info">
-                            <h1 className="chat__name">CR7</h1>
-                            <span className="last__message">*ejemplo de mensaje*</span>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-            </div>
+            <RecentChat/>
             <div className="chat">
                 <h1 className="title__dashboard">user/grupo</h1>
             </div>
