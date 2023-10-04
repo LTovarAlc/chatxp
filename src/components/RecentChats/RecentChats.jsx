4de6ae709d7a71ce03e10db247c0afc873371c7a
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-10-04 08:58:29 -0600

    add button addFriends

--- src/components/RecentChats/RecentChats.jsx
+++ src/components/RecentChats/RecentChats.jsx
@@ -16,6 +16,7 @@ const RecentChat = () => {
           <img src="./img/search.png" alt="Search" className="search" />
         </div>
         <div className="conversation__container">
+          <button className="addFriend" title="Add Friend/chat">+</button>
           <div className="chat__container">
             <img src="./img/user.png" alt="User" className="user__image" />
             <div className="chat__info">
