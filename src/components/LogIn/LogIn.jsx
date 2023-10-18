c7d4f965f25f896d7b3086be66df15155b5cad05
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-10-18 09:24:56 -0600

    show/hide password in login

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { useUser } from "../../AuthContext";
 
 import "./LogIn.css";
@@ -9,13 +9,18 @@ const LogIn = () => {
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // Nuevo estado
   const navigate = useNavigate();
   const { setUser } = useUser();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // validations
+    // Validaciones
     if (email === "" || password === "") {
       setEmailError("Email cannot be empty.");
       setPasswordError("Password cannot be empty.");
@@ -29,9 +34,9 @@ const LogIn = () => {
       const data = await response.json();
 
       if (response.ok && data.length > 0) {
-        const loggedInUser = data[0]
-        setUser(loggedInUser.username) // remplaza username por el nombre del usuario logeado
-        navigate("/dashboard")
+        const loggedInUser = data[0];
+        setUser(loggedInUser.username);
+        navigate("/dashboard");
       } else {
         setEmailError("Invalid email or password.");
         setPasswordError("Invalid email or password.");
@@ -59,7 +64,7 @@ const LogIn = () => {
 
       <div className="input__container">
         <input
-          type="password"
+          type={showPassword ? "text" : "password"} // Cambia el tipo de input
           placeholder="Password"
           className="input"
           value={password}
@@ -70,6 +75,11 @@ const LogIn = () => {
         </span>
       </div>
       <span className="error">{passwordError}</span>
+
+      {/* Botón para mostrar/ocultar contraseña */}
+      <span className="password-toggle" onClick={togglePasswordVisibility}>
+        {showPassword ? "Hide Password" : "Show Password"}
+      </span>
 
       <button className="btn__signup" type="submit">
         Log In
